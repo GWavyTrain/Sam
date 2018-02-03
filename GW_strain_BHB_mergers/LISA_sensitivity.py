89794f458eb6da0bd790f5a929f8c432fd797b8b
Sam Dunham <samueljdunham@gmail.com>
2018-02-03 09:37:21 -0600

    Added LISA sensitivity and strain calculator

---
+++ GW_strain_BHB_mergers/LISA_sensitivity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+f , h = np.loadtxt( 'LISA_sensitivity_curve.dat' , unpack = True )
+
+fig = plt.figure()
+plt.title( r'LISA Sensitivity Curve from http://www.srl.caltech.edu/~shane/sensitivity/MakeCurve.html' )
+plt.loglog( f , h , 'k-' )
+plt.xlabel( r'$f\,\left[Hz\right]$' )
+plt.ylabel( r'$h_{f}\,\left[Hz^{-1/2}\right]$' )
+plt.show()
+
