8368605c33563754677255bdb631abbead226649
Sam Dunham <samueljdunham@gmail.com>
2018-02-06 08:35:33 -0600

    ~/Desktop/Research/GW/Sam folder backup on Tue Feb  6 08:35:33 CST 2018

--- GW_strain_BHB_mergers/LISA_sensitivity.py
+++ GW_strain_BHB_mergers/LISA_sensitivity.py
@@ -3,16 +3,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-f , h = np.loadtxt( 'LISA_sensitivity_curve.dat' , unpack = True )
+f , h       = np.loadtxt( 'LISA_sensitivity.dat' , unpack = True )
+f_26 , h_26 = np.loadtxt( 'GW_strain_26.dat' , unpack = True )
 
-print np.diff(f)
-
-'''
 fig = plt.figure()
 plt.title( r'LISA Sensitivity Curve from http://www.srl.caltech.edu/~shane/sensitivity/MakeCurve.html' )
+
 plt.loglog( f , h , 'k-' )
+plt.loglog( f_26 , h_26 , 'r-' )
+
 plt.xlabel( r'$f\,\left[Hz\right]$' )
 plt.ylabel( r'$h_{f}\,\left[Hz^{-1/2}\right]$' )
-plt.savefig( 'LISA_sensitivity_curve.png' )
-#plt.show()
-'''
+
+#plt.savefig( 'LISA_sensitivity_curve.png' )
+plt.show()
