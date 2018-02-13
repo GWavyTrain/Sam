3862edae18b7eb4d67711b4289ef86c3efa5f9fc
Sam Dunham <samueljdunham@gmail.com>
2018-02-13 14:00:15 -0600

    ~/Desktop/Research/GW/Sam folder backup on Tue Feb 13 14:00:15 CST 2018

--- LISA_GOAT_plot.py
+++ LISA_GOAT_plot.py
@@ -8,12 +8,12 @@ f_LISA , h_LISA = np.loadtxt( 'LISA_sensitivity.dat' , unpack = True )
 fig = plt.figure()
 plt.title( r'LISA Sensitivity Curve from http://www.srl.caltech.edu/\textasciitilde shane/sensitivity/MakeCurve.html' )
 
-plt.loglog( f_LISA , f_LISA * h_LISA , 'k-' )
+plt.loglog( f_LISA , h_LISA , 'k-' )
 
 plt.xlabel( r'$f\,\left[Hz\right]$' )
-plt.ylabel( r'$f\,h\left(f\right)\,\left[Hz\right]$' )
+plt.ylabel( r'Root Spectral Density $\left[Hz^{-1/2}\right]$' )
 
 plt.xlim( 1.0e-5  , 2.0     )
-#plt.ylim( 1.0e-21 , 1.0e-16 )
+plt.ylim( 1.0e-21 , 1.0e-16 )
 
 plt.show()
