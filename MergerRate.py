c706e243c32c55bc3b7b880c56925b7aab87df7d
Sam Dunham <samueljdunham@gmail.com>
2018-04-09 14:00:47 -0500

    ~/Desktop/Research/GW/Sam folder backup on Mon Apr  9 14:00:47 CDT 2018

---
+++ MergerRate.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import numpy as np
+import matplotlib.pyplot as plt
+np.seterr( divide='ignore' )
+
+bins , rates = np.loadtxt( 'MergerRate.dat' , unpack = True )
+
+plt.step( bins , np.log10( rates ) , 'k' )
+plt.xlabel( r'$t_{Lookback\ to\ merger}\,\left[Gyr\right]$' )
+plt.ylabel( r'$\ell og_{10}\left(Merger\ Rate\,\left[Gyr^{-1}\right]\right)$' )
+
+plt.title( 'Total merger rate vs. lookback time for BBH mergers' )
+plt.show()
