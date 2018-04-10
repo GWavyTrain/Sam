14969c014ae27abe8f51f241c65462ae25ba22e6
Sam Dunham <samueljdunham@gmail.com>
2018-04-10 14:00:17 -0500

    ~/Desktop/Research/GW/Sam folder backup on Tue Apr 10 14:00:16 CDT 2018

--- MergerRate.py
+++ MergerRate.py
@@ -10,5 +10,7 @@ plt.step( bins , np.log10( rates ) , 'k' )
 plt.xlabel( r'$t_{Lookback\ to\ merger}\,\left[Gyr\right]$' )
 plt.ylabel( r'$\ell og_{10}\left(Merger\ Rate\,\left[Gyr^{-1}\right]\right)$' )
 
-plt.title( 'Total merger rate vs. lookback time for BBH mergers' )
-plt.show()
+plt.title( 'Total merger rate vs. lookback time for BBH mergers\
+\nRedshift Snapshots 101-110' )
+plt.savefig( 'TotalMergerRate.png' )
+#plt.show()
