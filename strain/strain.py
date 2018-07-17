f3ebc4d977c8fb41d23255ba145fa0757d921f27
Sam Dunham <samueljdunham@gmail.com>
2018-07-17 14:00:16 -0400

    ~/Research/GW/Sam folder backup on Tue Jul 17 14:00:15 EDT 2018

--- strain/strain.py
+++ strain/strain.py
@@ -6,38 +6,42 @@ from scipy.integrate import romberg
 # --- Define root directory ---
 BasePath = '/Users/sam/Research/GW/Sam/'
 
-# --- Physical constants ---
-G = 6.67e-11
-c = 2.99792458e8
+# --- All physical quantities are in SI units ---
 
-# --- Problem-specific constants ---
-H0   = 70.4 / 3.086e19
+# --- Physical constants ---
+G    = 6.67e-11
+c    = 2.99792458e8
 year = 86400.0 * 365.25
-tau  = 4.0 * year # LISA lifetime
-Msun = 2.0e30
 Gyr  = 1.0e9 * year
 Mpc  = 3.086e22
+Msun = 2.0e30
+
+# --- Problem-specific constants ---
+LISA_Lifetime = 4.0 * year
+tau           = LISA_Lifetime
 
 # --- Cosmological parameters from Mapelli et al., (2017), MNRAS, 472, 2422
-#     (WMAP-9, Hinshaw et al., (2013)) ---
+#     (WMAP-9, Hinshaw et al., (2013), ApJS, 208, 19) ---
+# NOTE: Hinshaw et al. values do NOT agree with those quoted in Mapelli
+h      = 0.704
+H0     = h * 100.0 / 3.086e19
 OmegaM = 0.2726
 OmegaL = 0.7274
-OmegaB = 0.0#456
+OmegaB = 0.0456
 
 # --- Read in LISA data ---
 LISA_data = loadtxt( BasePath + 'LISA_sensitivity.dat' )
-f       = LISA_data[:,0]
-hc_LISA = sqrt( LISA_data[:,1] * f )
+f         = LISA_data[:,0]
+hc_LISA   = sqrt( LISA_data[:,1] * f )
 
-# --- Compute comoving distance given lookback time ---
+# --- Compute comoving distance, given lookback time ---
 # Find redshift from lookback time, then find comoving distance from redshift
 
-# --- To use astropy.cosmology.z_at_value, need to have function
-#     that computes lookback time given redshift ---
+# astropy.cosmology.z_at_value requires function
+# that computes lookback time, given redshift
 
-# --- Compute lookback time given redshift ---
 def ComputeLookbackTimeFromRedshift( z ):
-    def LookbackTimeIntegrand(z):
+    def LookbackTimeIntegrand( z ):
         return 1.0 / ( ( 1.0 + z ) \
                        * sqrt( ( OmegaM + OmegaB ) * ( 1.0 + z )**3 + OmegaL ) )
 
@@ -52,13 +56,16 @@ def ComputeComovingDistanceFromRedshift( z ):
 
     return c / H0 * romberg( ComovingDistanceIntegrand, 0.0, z )
 
+# --- Combine above two functions to compute comoving
+#     distance from lookback time ---
 def ComputeComovingDistanceFromLookbackTime( tLb ):
     z = ComputeRedshiftFromLookbackTime( tLb )
     return ComputeComovingDistanceFromRedshift( z )
 
-# --- Characteristic strain from Sesana, et al., (2005), ApJ, 623, 23 ---
+# --- Characteristic strain from Sesana et al., (2005), ApJ, 623, 23 ---
 def ComputeCharacteristicStrain( M1, M2, r ):
 
+    # --- Initialize array to hold characteristic strains ---
     hc = empty( len( f ), float )
     
     # --- Compute chirp mass ---
@@ -82,18 +89,19 @@ def ComputeCharacteristicStrain( M1, M2, r ):
 
     return hc
 
-# --- Test characteristic strain calculation: GOAT, Figure 2.1 ---
+# --- Test characteristic strain calculation: Sesana (2016), PRL, 116, 231102
+#     (GW150914) ---
 import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
 
-M1 = 1.0e7 * Msun
-M2 = M1
-z = 3.0
+M1 = 36.0 * Msun
+M2 = 29.0 * Msun
+z  = 0.09
 
-ax.loglog( f, hc_LISA, 'k-' )
+#ax.loglog( f, hc_LISA, 'k-' )
 ax.loglog( f, ComputeCharacteristicStrain \
                  ( M1, M2, ComputeComovingDistanceFromRedshift( z ) ) )
-ax.set_xlim( 1.0e-5, 2.0e0 )
-ax.set_ylim( 1.0e-21, 1.0e-16 )
+ax.set_xlim( 1.0e-3, 3.0e3 )
+ax.set_ylim( 1.0e-23, 1.0e-18 )
 
 plt.show()
